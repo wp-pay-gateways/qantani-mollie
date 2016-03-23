813a182d51a491d69bb056f33cbdfef343b8c12c
Remco Tolsma <info@remcotolsma.nl>
2016-03-23 10:36:39 +0100

    Getting ready for version 1.0.1.

--- src/Integration.php
+++ src/Integration.php
@@ -7,7 +7,8 @@
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.0.1
+ * @since 1.0.0
  */
 class Pronamic_WP_Pay_Gateways_Qantani_Mollie_Integration extends Pronamic_WP_Pay_Gateways_Mollie_Integration {
 	public function __construct() {
